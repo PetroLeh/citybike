874e8979a7ea2e856a70e30a5e9d9bcfbdfc2bb9
Petro Lehtonen <petro.lehtonen@gmail.com>
2023-03-17 12:11:52 +0200

    initial commit

---
+++ src/test/java/citybike/citybike/CitybikeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package citybike.citybike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitybikeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
